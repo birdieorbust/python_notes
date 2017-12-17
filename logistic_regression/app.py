eb0310b8fd0b57d893d7945e60b854cdcfa44fca
sam <sam.dc.hall@gmail.com>
2017-12-17 20:57:53 +0000

    Dockerised flask app

--- logistic_regression/app.py
+++ logistic_regression/app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, render_template
 from sklearn.externals import joblib
 import pandas as pd
 import numpy
-
+import logging
+from logging.handlers import RotatingFileHandler
+import json
 
 app = Flask(__name__)
 
@@ -14,31 +16,42 @@ def index():
 @app.route('/getresponse',methods=['POST','GET'])
 def getresponse():
 	if request.method=='POST':
-		form_values=request.json
+		
 
-		classifier = joblib.load('model.pkl')
-		model_columns = joblib.load('model_columns.pkl')
-		#hardcode values for now
-		form_values=[{'age':45,'yrs_married':10}]
+		form_values=request.form.to_dict()
+		app.logger.info(form_values)
+		classifier = joblib.load('models/model.pkl')
+		model_columns = joblib.load('models/model_columns.pkl')
+		app.logger.info("Taking post request form details and cleaning to match model input"),
 		#take the json and turn into a pandas data frame
-		query_df = pd.DataFrame(form_values)
+		d=[{'age':form_values['age'],'yrs_married':form_values['yrs_married']}]
+		query_df = pd.DataFrame(data=d)
 		#get dummies for any categorical variables
-		query = pd.get_dummies(query_df)
-		print('VECTOR FOR PREDICTION')
-		print(query)
+		#query = pd.get_dummies(query_df)
+
+		print(query_df)
+		#print(model_columns.columns)
+
+
 
 
 		for col in model_columns:
-			if col not in query.columns:
-				query[col] = 0
+			if col not in query_df.columns:
+				query_df[col] = 0
 
-		print(query)
+		app.logger.info("Predicting outcome response given inputs")
 
-		prediction = classifier.predict(query)
+		prediction = classifier.predict(query_df)
+		app.logger.info('RESPONSE PREDICTION  = %s',str(prediction))
 		return render_template('result.html',prediction=prediction)
 
 
 
 if __name__ == '__main__':
 
-	app.run(debug=True)
+	app.run(debug=True,host='0.0.0.0')
+	LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+	LOGGING_LEVEL  = logging.DEBUG
+	formatter = logging.Formatter(app.config['LOGGING_FORMAT'])
+	handler.setFormatter(formatter)
+	app.logger.addHandler(handler)
--- logistic_regression/create_model.py
+++ logistic_regression/create_model.py
@@ -4,6 +4,7 @@ import statsmodels.api as sm
 from sklearn import preprocessing
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.externals import joblib
 import pickle
 
 # load dataset
@@ -11,17 +12,15 @@ dta = sm.datasets.fair.load_pandas().data
 # add "affair" column: 1 represents having affairs, 0 represents not. The original column is the measure of time spent in extramarital affairs
 dta['affair'] = (dta.affairs > 0).astype(int)
 
-
+#create dummies for occupation
 dataforregression=pd.get_dummies(dta, columns =['occupation'])
 
-
-#specify columns for model FOR NOW JUST SPECIFY AGE
-X = dataforregression.iloc[:,1:3]
+X = dataforregression.iloc[:,0:]
 print(X.head())
 #specify column with labels in
 y = dataforregression.iloc[:,8]
 #drop response column from predictors
-#X=X.drop(['affair'],axis=1)
+X=X.drop(['affair'],axis=1)
 #function form sklearn to split data into training and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
@@ -29,5 +28,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 classifier = LogisticRegression(random_state=0)
 classifier.fit(X_train, y_train)
 
-with open('affairslogistic.pkl', 'wb') as fid:
-    pickle.dump(classifier, fid,2) 
+
+model_columns = list(X.columns)
+print(model_columns)
+
+#SAVE TO DISK
+joblib.dump(classifier, 'models/model.pkl')
+joblib.dump(model_columns, 'models/model_columns.pkl')
+
